6536f32096e8bc84c1f7d84002d44aeeeedddd5b
StevenSTidd <7004555203@GSA.GOV>
2023-01-09 08:21:13 -0500

    created InvoiceService class

---
+++ src/main/java/com/steventidd/MyFancyPdfInvoicesServlet.java
@@ -0,0 +1,47 @@
+package com.steventidd;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyFancyPdfInvoicesServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        if (request.getRequestURI().equals("/")) {
+            response.setContentType("text/html; charset=UTF-8");
+            response.getWriter().print(
+                    "<html>\n" +
+                            "<body>\n" +
+                            "<h1>Hello World</h1>\n" +
+                            "<p>This is my very first, embedded Tomcat, HTML Page!</p>\n" +
+                            "</body>\n" +
+                            "</html>");
+        } else if (request.getRequestURI().equals("/invoices")) {
+            response.setContentType("application/json; charset=UTF-8");
+            response.getWriter().print("[]");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (request.getRequestURI().equalsIgnoreCase("/invoices")) {
+
+            String userId = request.getParameter("user_id");
+            Integer amount = Integer.parseInt(request.getParameter("amount"));
+
+            Invoice invoice = new InvoiceService().create(userId, amount);
+
+            response.setContentType("application/json; charset=UTF-8");
+            String json = new ObjectMapper().writeValueAsString(invoice);
+            response.getWriter().print(json);
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+}
