d41bf5a2270bcf07657c8d577a554ae8e603da06
Steven Tidd <steven.tidd@gmail.com>
2023-03-13 07:38:09 -0400

    saving work

---
+++ src/main/java/com/steventidd/web/WebsiteController.java
@@ -0,0 +1,37 @@
+package com.steventidd.web;
+
+import com.steventidd.web.forms.LoginForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+
+@Controller
+public class WebsiteController {
+
+    @GetMapping("/")
+    public String homepage(Model model, @RequestParam(required = false, defaultValue = "stranger") String username){
+        model.addAttribute("username", username);
+        model.addAttribute("currentDate", new Date());
+        return "index.html";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("loginForm", new LoginForm());
+        return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm loginForm, Model model){
+        if (loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        model.addAttribute("invalidCredentials", "true");
+        return "login.html";
+    }
+}
---
+++ src/main/java/com/steventidd/web/forms/LoginForm.java
@@ -0,0 +1,23 @@
+package com.steventidd.web.forms;
+
+public class LoginForm {
+
+    private String username, password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
